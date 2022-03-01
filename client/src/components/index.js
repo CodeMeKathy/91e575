c4ecbe73c7d09393557fa9fba15a874b6d439721
CodeMeKathy * <Kathy@KathyLambert.me>
2022-02-28 22:09:42 -0500

    Feat (index.js): Create and add <FormHeader /> with styles

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,4 @@ export { default as Home } from "./Home";
 export { default as SnackbarError } from "./SnackbarError";
 export { default as SidePanel } from "./SidePanel";
 export { default as FormInput } from "./FormInput";
+export { default as FormHeader } from "./FormHeader";
