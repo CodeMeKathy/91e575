b8b7db79a372509e726b8bb457a1873928a54c4a
CodeMeKathy * <Kathy@KathyLambert.me>
2022-03-06 23:35:04 -0500

    Feat (index.js): Create and add <TextBubble /> with styles

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as SnackbarError } from "./SnackbarError";
 export { default as SidePanel } from "./SidePanel";
+export { default as TextBubble } from "./ActiveChat/TextBubble";
