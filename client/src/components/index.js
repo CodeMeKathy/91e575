5c5c15ac3e8435aff88692a716b7af46e97a7271
CodeMeKathy * <Kathy@KathyLambert.me>
2022-03-06 23:37:47 -0500

    Feat (index.js): Create and add <ImagesBubble /> with styles

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Home } from "./Home";
 export { default as SnackbarError } from "./SnackbarError";
 export { default as SidePanel } from "./SidePanel";
+export { default as ImagesBubble } from "./ActiveChat/ImagesBubble";
 export { default as ImageTextBubble } from "./ActiveChat/ImageTextBubble";
 export { default as TextBubble } from "./ActiveChat/TextBubble";
